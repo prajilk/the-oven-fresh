68b019435d6444cf8bc7a28780b96b14f2c7427a
Prajil K <prajilk20017@gmail.com>
2025-10-16 11:53:53 +0530

    fixed middleware redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,11 @@
 import { type NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const cookie = request.cookies.get('better-auth.session_token');
+  const cookie =
+    request.cookies.get('__Secure-better-auth.session_token') ??
+    request.cookies.get('better-auth.session_token');
   if (!cookie || cookie.value) {
-    return NextResponse.redirect(new URL('/sign-in', request.url));
+    return NextResponse.redirect(new URL('/', request.url));
   }
   return NextResponse.next();
 }
